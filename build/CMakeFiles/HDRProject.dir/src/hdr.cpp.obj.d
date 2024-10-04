4a9bdca06e0f9d20e5082cf2ebe81df359ef4845
MaRuKo_8 <marcolbilli@gmail.com>
2024-10-04 10:52:30 +0200

    Builded with CMake

--- build/CMakeFiles/HDRProject.dir/src/hdr.cpp.obj.d
+++ build/CMakeFiles/HDRProject.dir/src/hdr.cpp.obj.d
@@ -200,68 +200,11 @@ CMakeFiles/HDRProject.dir/src/hdr.cpp.obj: \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glad/glad.h \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/KHR/khrplatform.h \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/GLFW/glfw3.h \
- C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/stb/stb_image.h \
- C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h \
- C:/msys64/ucrt64/include/stdarg.h \
- C:/msys64/ucrt64/include/_mingw_stdarg.h \
- C:/msys64/ucrt64/include/string.h \
- C:/msys64/ucrt64/include/sec_api/string_s.h \
- C:/msys64/ucrt64/include/c++/13.2.0/math.h \
- C:/msys64/ucrt64/include/assert.h \
- C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/emmintrin.h \
- C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/xmmintrin.h \
- C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mmintrin.h \
- C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/nlohmann/json.hpp \
- C:/msys64/ucrt64/include/c++/13.2.0/functional \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/std_function.h \
- C:/msys64/ucrt64/include/c++/13.2.0/unordered_map \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/unordered_map.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/hashtable.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/hashtable_policy.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/enable_special_members.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/node_handle.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/erase_if.h \
- C:/msys64/ucrt64/include/c++/13.2.0/array \
- C:/msys64/ucrt64/include/c++/13.2.0/compare \
- C:/msys64/ucrt64/include/c++/13.2.0/iterator \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stream_iterator.h \
- C:/msys64/ucrt64/include/c++/13.2.0/memory \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_raw_storage_iter.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/align.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/shared_ptr_atomic.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/atomic_base.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/atomic_lockfree_defines.h \
- C:/msys64/ucrt64/include/c++/13.2.0/backward/auto_ptr.h \
- C:/msys64/ucrt64/include/c++/13.2.0/pstl/glue_memory_defs.h \
- C:/msys64/ucrt64/include/c++/13.2.0/utility \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_relops.h \
- C:/msys64/ucrt64/include/c++/13.2.0/forward_list \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/forward_list.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/forward_list.tcc \
- C:/msys64/ucrt64/include/c++/13.2.0/map \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_tree.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_map.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_multimap.h \
- C:/msys64/ucrt64/include/c++/13.2.0/valarray \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_array.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_array.tcc \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_before.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/slice_array.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_after.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/gslice.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/gslice_array.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/mask_array.h \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/indirect_array.h \
- C:/msys64/ucrt64/include/c++/13.2.0/version \
- C:/msys64/ucrt64/include/c++/13.2.0/cassert \
- C:/msys64/ucrt64/include/c++/13.2.0/cstring \
- C:/msys64/ucrt64/include/c++/13.2.0/numeric \
- C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_numeric.h \
- C:/msys64/ucrt64/include/c++/13.2.0/pstl/glue_numeric_defs.h \
- C:/msys64/ucrt64/include/c++/13.2.0/any \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/glm.hpp \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/detail/_fixes.hpp \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/detail/setup.hpp \
+ C:/msys64/ucrt64/include/c++/13.2.0/cassert \
+ C:/msys64/ucrt64/include/assert.h \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/detail/../simd/platform.h \
  C:/msys64/ucrt64/include/c++/13.2.0/climits \
  C:/msys64/ucrt64/include/c++/13.2.0/cfloat \
@@ -452,6 +395,63 @@ CMakeFiles/HDRProject.dir/src/hdr.cpp.obj: \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/gtc/../gtc/../ext/vector_int1_sized.hpp \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/gtc/../gtc/../ext/vector_uint1.hpp \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/gtc/../gtc/../ext/vector_uint1_sized.hpp \
+ C:/msys64/ucrt64/include/c++/13.2.0/cstring \
+ C:/msys64/ucrt64/include/string.h \
+ C:/msys64/ucrt64/include/sec_api/string_s.h \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/glm/gtc/type_ptr.inl \
+ C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/stb/stb_image.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/stdarg.h \
+ C:/msys64/ucrt64/include/stdarg.h \
+ C:/msys64/ucrt64/include/_mingw_stdarg.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/math.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/emmintrin.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/xmmintrin.h \
+ C:/msys64/ucrt64/lib/gcc/x86_64-w64-mingw32/13.2.0/include/mmintrin.h \
+ C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/nlohmann/json.hpp \
+ C:/msys64/ucrt64/include/c++/13.2.0/functional \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/std_function.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/unordered_map \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/unordered_map.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/hashtable.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/hashtable_policy.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/enable_special_members.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/node_handle.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/erase_if.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/array \
+ C:/msys64/ucrt64/include/c++/13.2.0/compare \
+ C:/msys64/ucrt64/include/c++/13.2.0/iterator \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stream_iterator.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/memory \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_raw_storage_iter.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/align.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/shared_ptr_atomic.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/atomic_base.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/atomic_lockfree_defines.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/backward/auto_ptr.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/pstl/glue_memory_defs.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/utility \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_relops.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/forward_list \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/forward_list.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/forward_list.tcc \
+ C:/msys64/ucrt64/include/c++/13.2.0/map \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_tree.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_map.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_multimap.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/valarray \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_array.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_array.tcc \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_before.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/slice_array.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/valarray_after.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/gslice.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/gslice_array.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/mask_array.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/indirect_array.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/version \
+ C:/msys64/ucrt64/include/c++/13.2.0/numeric \
+ C:/msys64/ucrt64/include/c++/13.2.0/bits/stl_numeric.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/pstl/glue_numeric_defs.h \
+ C:/msys64/ucrt64/include/c++/13.2.0/any \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/shader.h \
  C:/Users/marco/OneDrive/Desktop/COMPUT~1/progetto/HDR_RE~1/include/camera.h
